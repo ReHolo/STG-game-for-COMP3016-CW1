a78084165fad432daca437391fa2f352f707e889
sirun <ww1228747899@outlook.com>
2024-11-04 11:31:55 +0000

    SDL relative position

--- STG game/main.cpp
+++ STG game/main.cpp
@@ -1,8 +1,8 @@
 #define SDL_MAIN_HANDLED
 #include <iostream>
 #include <SDL.h>
-#include <SDL_ttf.h>
 #include <SDL_image.h>
+#include <SDL_ttf.h>
 #include <vector>
 #include <string>
 #include <cstdlib>
